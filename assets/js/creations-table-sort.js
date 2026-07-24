0f1aafa5c0454232536512ce3714fce3456e857f
TownGG <tgg508034@gmail.com>
2026-07-24 13:44:36 +0800

    Default Creations total table sort by daily

--- assets/js/creations-table-sort.js
+++ assets/js/creations-table-sort.js
@@ -5,7 +5,7 @@
       tableName: 'creations',
       buttonAttr: 'data-creations-table-sort',
       ready: (table) => table.dataset.creationsDailyReady === 'true',
-      defaultSort: { key: 'downloads', direction: 'desc' },
+      defaultSort: { key: 'daily', direction: 'desc' },
       columns: [
         { key: 'creation', type: 'text' },
         { key: 'daily', type: 'number' },
